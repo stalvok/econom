1049d4f4e9cef5135843c15408f5b45139d184ec
stalvok <80374859+stalvok@users.noreply.github.com>
2024-09-17 14:38:43 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    colors:{
+      primary: '#F95707',
+    },
+    extend: {
+      fontFamily: {
+        sans: [
+          'Open Sans',
+          'ui-sans-serif',
+          'system-ui',
+          '-apple-system',
+          'system-ui',
+          'Segoe UI',
+          'Roboto',
+          'Helvetica Neue',
+          'Arial',
+          'Noto Sans',
+          'sans-serif',
+          'Apple Color Emoji',
+          'Segoe UI Emoji',
+          'Segoe UI Symbol',
+          'Noto Color Emoji'
+        ]
+      },
+    },
+  },
+  plugins: [],
+}
+
