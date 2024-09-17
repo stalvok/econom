f8ae640b18a9cc768d235ee7beeeb46ec94f25f2
stalvok <80374859+stalvok@users.noreply.github.com>
2024-09-17 14:43:20 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Main/>
+</template>
+
+<script lang="ts">
+import Main from "./pages/main.vue";
+export default {
+  components: {Main},
+  props: {},
+  setup() {
+    return {}
+  }
+}
+</script>
---
+++ src/pages/main.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="w-full relative">
+    <hr class="border-[#E8E8E8] w-full border absolute top-[112px]">
+    <div class="max-w-[1296px] px-12 mx-auto">
+      <AppHeader class=" "/>
+    </div>
+    <div class="w-full relative h-[689px] bg-[#f3f7f4]">
+      <div class="w-[838px] absolute right-0  h-[689px]">
+        <img src="/images/truck.webp" height="100%" width="100%" class="w-full h-full" />
+      </div>
+      <div class="w-[200px] absolute left-1/2 ml-10 top-[52px] -translate-x-1/2 h-[200px]">
+        <img src="/images/video.webp" height="100%" width="100%" />
+      </div>
+      <div class="max-w-[1296px] relative  pt-[100px] pb-[100px] px-12 mx-auto">
+        <div class="flex flex-col items-start max-w-[512px] gap-[50px]">
+          <span class="font-semibold main-title text-[48px]">Приём лома чёрных <br/>и цветных металлов <br><span class="text-[#105720]">в Санкт-Петербурге </span></span>
+          <div class="flex flex-wrap gap-3">
+            <div class="button outline-btn">
+              Цены на сегодня
+            </div>
+            <div class="button outline-btn">
+              Как сдать металл
+            </div>
+            <div class="button outline-btn">
+              Отзывы
+            </div>
+            <div class="button outline-btn">
+              Акции
+            </div>
+          </div>
+          <div class="button default mt-6">Оставить заявку</div>
+      </div>
+    </div>
+      <div class="flex flex-col mx-auto max-w-[1296px] px-12">
+        <div class="items-center w-full justify-between flex gap-6 mt-[62px]">
+          <div class="flex flex-col border rounded-xl border-[#E8E8E8] items-start pt-5 pb-6 px-[30px] gap-2.5">
+            <div class="flex items-center gap-3">
+              <svg width="52" height="52" viewBox="0 0 52 52" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="26" cy="26" r="26" fill="#105720"/>
+                <path d="M35 18H17C15.8954 18 15 18.8954 15 20V32C15 33.1046 15.8954 34 17 34H35C36.1046 34 37 33.1046 37 32V20C37 18.8954 36.1046 18 35 18Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M15 24H37" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+              </svg>
+              <span class="font-semibold text-xl">Моментальный расчёт</span>
+            </div>
+            <span>Мгновенная оплата по безналичному <br> расчету на месте приёма</span>
+          </div>
+          <div class="flex flex-col border rounded-xl border-[#E8E8E8] items-start pt-5 pb-6 px-[30px] gap-2.5">
+            <div class="flex items-center gap-3">
+              <svg width="52" height="52" viewBox="0 0 52 52" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="26" cy="26" r="26" fill="#105720"/>
+                <path d="M37 20L27.5 29.5L22.5 24.5L15 32" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M31 20H37V26" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+              </svg>
+              <span class="font-semibold text-xl">Высокая цена закупки</span>
+            </div>
+            <span>Максимальная высокая цена закупки лома <br> на рынке Санкт-Петербурга</span>
+          </div>
+          <div class="flex flex-col border rounded-xl border-[#E8E8E8] items-start pt-5 pb-6 px-[30px] gap-2.5">
+            <div class="flex items-center gap-3">
+              <svg width="52" height="52" viewBox="0 0 52 52" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="26" cy="26" r="26" fill="#105720"/>
+                <path d="M30 17H15V30H30V17Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M30 22H34L37 25V30H30V22Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M19.5 35C20.8807 35 22 33.8807 22 32.5C22 31.1193 20.8807 30 19.5 30C18.1193 30 17 31.1193 17 32.5C17 33.8807 18.1193 35 19.5 35Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M32.5 35C33.8807 35 35 33.8807 35 32.5C35 31.1193 33.8807 30 32.5 30C31.1193 30 30 31.1193 30 32.5C30 33.8807 31.1193 35 32.5 35Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+              </svg>
+              <span class="font-semibold text-xl">Собственный автопарк</span>
+            </div>
+            <span>Свой автопарк, в составе которого <br> более 30 единиц спецтехники</span>
+          </div>
+        </div>
+        <div class="mt-[100px] mb-20 justify-between items-start flex border-y-2 border-primary py-11">
+          <span class="text-[42px] font-semibold">Комплексный <br> сервис</span>
+          <span class="flex flex-col max-w-[746px] gap-8">
+            <p>
+              Компания «ЭКОЛОМ» — ваш надежный партнер по переработке металлолома
+              в Санкт-Петербурге. Мы стремимся к экологической ответственности,
+              предлагая выгодные условия и профессиональный сервис на всех этапах работы.
+              Гарантируем честность, прозрачность и высокие цены на металлолом!
+            </p>
+            <p>
+              Помимо переработки лома, наша компания предлагает комплексные услуги, включая оценку состава металлов, вывоз и демонтаж.
+              Мы осуществляем демонтаж любой сложности – от кораблей и зданий до железнодорожных составов и путей.Наши опытные специалисты
+              и собственный автопарк обеспечивают оперативность и надежность в выполнении задач.
+            </p>
+            <p>
+              Вместе с компанией «ЭКОЛОМ» вы получаете не только надежного партнера по переработке металлолома,
+              но и комплексный сервис высочайшего уровня.
+              Мы с удовольствием берем
+              на себя все этапы работы — от приёмки до вывоза и демонтажа.
+            </p>
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+import AppHeader from "../components/AppHeader.vue";
+
+export default defineComponent({
+  name: "MainPage",
+  components:{ AppHeader }
+})
+</script>
+<style >
+  input::placeholder {
+    color: #F95707;
+  }
+  .button {
+    @apply flex duration-300 items-center  cursor-pointer rounded-full justify-center py-3 px-8 font-semibold;
+    &.default {
+       @apply text-[#FFFFFF] bg-primary hover:bg-[#fa854b];
+    }
+    &.outline-btn {
+      @apply border-primary text-primary border hover:bg-[#f2d8cb] ;
+    }
+  }
+</style>
